5fc5690620fe28b35b8d95626976b5a62917cb64
Lambda_School_Loaner_212 <lambda_school_loaner_212@Lambda-School-Loaner-212s-MacBook-Pro.local>
2020-01-21 08:10:53 -0800

    did not finish count sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,16 +1,28 @@
+n = len(arr) - 1
+
 # TO-DO: Complete the selection_sort() function below 
 def selection_sort( arr ):
     # loop through n-1 elements
-    for i in range(0, len(arr) - 1):
+    for i in range(0, n):
         cur_index = i
         smallest_index = cur_index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc) 
-             
+        if i < n:
+            j = i + 1
+            if j < n:
+                if arr[j] < arr[smallest_index]:
+                    smallest_index = j
+                    for j in range(0, n):
+                        j + 1
+            # TO-DO: swap
+            else:
+                arr[smallest_index], arr[i] = arr[i], arr[smallest_index]
+                return i + 1
 
 
 
-        # TO-DO: swap
+        
 
 
 
@@ -20,7 +32,20 @@ def selection_sort( arr ):
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
+    have_swapped = True
+        #i needs to be in zero index
+        #i[0]
+        while have_swapped:
+            have_swapped = False
+        #Compare to right hand neighbor
+        #i[0 + 1]
+        for i in range(0, n):
 
+        #If i[0+1] < i[0]:
+        if arr[i] > arr[i+1]:
+            arr[i], arr[i+1] = arr[i+1], arr[i]
+        ##arr[smallest_index], arr[i] = arr[i], arr[smallest_index]
+    
     return arr
 
 
