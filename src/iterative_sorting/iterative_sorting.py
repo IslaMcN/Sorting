58f1373249aec8923e4e63686581872e08267cac
Lambda_School_Loaner_212 <lambda_school_loaner_212@Lambda-School-Loaner-212s-MacBook-Pro.local>
2020-02-18 12:39:46 -0800

    Attempted stretch

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,24 +1,22 @@
-n = len(arr) - 1
+
 
 # TO-DO: Complete the selection_sort() function below 
 def selection_sort( arr ):
+    print(arr)
+    n = len(arr)
     # loop through n-1 elements
-    for i in range(0, n):
-        cur_index = i
-        smallest_index = cur_index
+    for i in range(n-1,0,-1):
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc) 
-        if i < n:
-            j = i + 1
-            if j < n:
-                if arr[j] < arr[smallest_index]:
-                    smallest_index = j
-                    for j in range(0, n):
-                        j + 1
-            # TO-DO: swap
-            else:
-                arr[smallest_index], arr[i] = arr[i], arr[smallest_index]
-                return i + 1
+        max = 0
+        for j in range(1, i+1):
+            if arr[j] > arr[max]:
+                max = j
+        temp = arr[i]
+        arr[i] = arr[max]
+        arr[max] = temp
+            
+               
 
 
 
@@ -32,24 +30,33 @@ def selection_sort( arr ):
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
-    have_swapped = True
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]: 
+                 arr[j], arr[j+1]= arr[j+1], arr[j]
         #i needs to be in zero index
         #i[0]
-        while have_swapped:
-            have_swapped = False
-        #Compare to right hand neighbor
-        #i[0 + 1]
-        for i in range(0, n):
-
-        #If i[0+1] < i[0]:
-        if arr[i] > arr[i+1]:
-            arr[i], arr[i+1] = arr[i+1], arr[i]
+    
         ##arr[smallest_index], arr[i] = arr[i], arr[smallest_index]
     
     return arr
 
 
 # STRETCH: implement the Count Sort function below
-def count_sort( arr, maximum=-1 ):
+def count_sort( arr, maximum=-1):
+    counts = [0] * (maximum + 1)
+    for item in arr:
+        counts[item] += 1
+    num_items_before = 0
+    for i, count in enumerate(counts):
+        counts[i] = num_items_before
+        num_items_before += count
+    sorted_list = [None] * len(arr)
+    for item in arr:
+        sorted_list[ counts[item] ] = item
+        counts[item] += 1
 
-    return arr
+    return sorted_list
+
+    
